3a84398e87b876fa4400864101abef18f4436eeb
Siam <83664154+shaongitbd@users.noreply.github.com>
2024-08-27 14:43:29 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/tiles/<zoom>/<x>/<y>', methods=['GET', 'POST'])
 def tiles(zoom, x, y):
     default = 'tiles/13/6142/3529.png' # this is a blank tile, change to whatever you want
-    filename = f'tiles/{zoom}/{x}/{y}.png' % (zoom, x, y)
+    filename = f'tiles/{zoom}/{x}/{y}.png'
     #if os.path.isfile(filename):
     #    return send_file(filename)
    # else:
