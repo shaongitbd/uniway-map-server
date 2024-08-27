337b9135e6351f40bb7541dfc63bd1415a77064b
Siam <83664154+shaongitbd@users.noreply.github.com>
2024-08-27 13:31:12 +0600

    fixed path bug

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, send_file
 app = Flask(__name__, static_url_path='/static')
 @app.route('/tiles/<zoom>/<y>/<x>', methods=['GET', 'POST'])
 def tiles(zoom, y, x):
-    default = 'tiles\\13\\6142\\3529.png' # this is a blank tile, change to whatever you want
-    filename = '\tiles\\%s\\%s\\%s.png' % (zoom, x, y)
+    default = 'tiles/13/6142/3529.png' # this is a blank tile, change to whatever you want
+    filename = 'tiles/%s/%s/%s.png' % (zoom, x, y)
     if os.path.isfile(filename):
         return send_file(filename)
     else:
